7f56fe8d73bb72abb36ff051814da7ec00a1d77f
Default <noreply@github.com>
2026-04-23 18:31:26 +0800

    Project import generated by Copybara.
    GitOrigin-RevId: af9d31b8ea10e03421f51f80b6b307e26ec77e2e

--- tests/integration/dbassert/doc.go
+++
@@ -1,4 +0,0 @@
-// Package dbassert provides database assertion helpers for integration tests.
-//
-// Run with: go test -tags=integration ./tests/integration/...
-package dbassert
--- tests/integration/doc.go
+++
@@ -1,5 +0,0 @@
-// Package integration provides integration tests that verify database state
-// after requests. These tests use real databases via the Docker CLI.
-//
-// Run with: go test -tags=integration ./tests/integration/...
-package integration
